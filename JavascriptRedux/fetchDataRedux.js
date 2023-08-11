ba82ca68697511a25e6461d53a2c419326958cd5
SWAPNIL AHMED SHISHIR <ahmed66882298@gmail.com>
2023-08-11 18:29:20 +0600

    this module learn data fetching redux-thunk

--- JavascriptRedux/fetchDataRedux.js
+++ JavascriptRedux/fetchDataRedux.js
@@ -1,19 +1,23 @@
+// import or required
+const { default: axios } = require('axios');
+const {createStore , applyMiddleware} = require('redux');
+const thunk = require('redux-thunk').default;
+
 // constants
+
 const GET_TODO_REQUIEST = 'GET_TODO_REQUIEST';
 const GET_TODO_SUCCESS = 'GET_TODO_SUCCESS';
 const GET_TODO_ERRO = 'GET_TODO_ERRO';
+const API_URL = "https://jsonplaceholder.typicode.com/todos";
 
 
 // state
 const initialTodoState = {
     todos:[],
     isLodding:false,
-    error:null 
+    erro:null 
 }
-// action
-
-
-// reducer 
+// action 
 const getTodoRequiest = () => {
     return {
         type: GET_TODO_REQUIEST
@@ -31,4 +35,60 @@ const getTodoErro = (erro) => {
         payload:erro
     }
 }
+
+// reducer
+const useReducer = (state=initialTodoState , action) => {
+    switch (action.type) {
+        case GET_TODO_REQUIEST:
+            return{
+                ...state,
+                isLodding:true
+            }   
+        case GET_TODO_SUCCESS:
+            return {
+                ...state,
+                isLodding:false,
+                todos: action.payload
+            }
+        case GET_TODO_ERRO:
+            return {
+                ...state,
+                isLodding:false,
+                erro:action.payload
+            }
+    
+        default:
+            return state;
+    }
+}
+// data fetching  async action create 
+const fetchDate = () => {
+    return (dispatch) => {
+        dispatch(getTodoRequiest());
+           axios.get(API_URL)
+           .then(res => {
+               const data = res.data;
+               const title = data.map( res => res.title);
+               dispatch(getTodoSuccess(title));
+           } )
+           .catch (erro => {
+               const erroMeassage = erro.message;
+               dispatch(getTodoErro(erroMeassage)) ;
+
+            
+            })
+      }
+}
+
+
 // store
+const store = createStore(
+    useReducer,
+    applyMiddleware(thunk)
+  );
+
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+store.dispatch(fetchDate());
